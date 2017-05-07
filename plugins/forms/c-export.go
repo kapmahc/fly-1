334c8e13aed26e25e1877d996510f4b0710b7115
Jeremy Zheng <jitang.zheng@gmail.com>
2017-05-06 23:27:01 -0700

    forms export

--- plugins/forms/c-export.go
+++ plugins/forms/c-export.go
@@ -1,8 +1,27 @@
 package forms
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/csv"
+	"fmt"
 
-// getFormExport html? ini?
+	"github.com/gin-gonic/gin"
+)
+
+// getFormExport csv?
 func (p *Plugin) getFormExport(c *gin.Context, l string) error {
+	item := c.MustGet("item").(*Form)
+	header, rows, err := p._exportForm(item)
+	if err != nil {
+		return err
+	}
+	c.Header("Content-Disposition", fmt.Sprintf("attachment; filename=form-%d.ini", item.ID))
+	c.Header("Content-Type", "text/plain; charset=utf-8")
+	wrt := csv.NewWriter(c.Writer)
+	wrt.Write(header)
+
+	for _, row := range rows {
+		wrt.Write(row)
+	}
+	wrt.Flush()
 	return nil
 }
--- plugins/forms/c-report.go
+++ plugins/forms/c-report.go
@@ -1,7 +1,54 @@
 package forms
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (p *Plugin) _exportForm(f *Form) ([]string, [][]string, error) {
+	var header []string
+	for _, f := range f.Fields {
+		header = append(header, f.Label)
+	}
+
+	var items [][]string
+	for _, r := range f.Records {
+		var row []string
+		val := make(map[string]interface{})
+		if err := json.Unmarshal([]byte(r.Value), &val); err != nil {
+			return nil, nil, err
+		}
+		for _, f := range f.Fields {
+			switch f.Name {
+			case "phone":
+				row = append(row, r.Phone)
+			case "email":
+				row = append(row, r.Email)
+			case "username":
+				row = append(row, r.Username)
+			default:
+				row = append(row, fmt.Sprintf("%+v", val[f.Name]))
+			}
+		}
+		items = append(items, row)
+	}
+
+	return header, items, nil
+}
 
 func (p *Plugin) getFormReport(c *gin.Context, l string) (gin.H, error) {
-	return gin.H{}, nil
+	item := c.MustGet("item").(*Form)
+	title := p.I18n.T(l, "buttons.report") + "-" + item.Title
+
+	header, rows, err := p._exportForm(item)
+	if err != nil {
+		return nil, err
+	}
+	return gin.H{
+		"title":  title,
+		"header": header,
+		"rows":   rows,
+	}, nil
 }
