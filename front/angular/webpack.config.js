97f63bc59d4f96fc5238c448e61e3e9afab5e6e8
Jeremy Zheng <jitang.zheng@gmail.com>
2017-05-15 13:56:37 -0700

    add webpack

---
+++ front/angular/webpack.config.js
@@ -0,0 +1,18 @@
+import {AotPlugin} from '@ngtools/webpack'
+
+exports = {
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: '@ngtools/webpack',
+      }
+    ]
+  },
+  plugins: [
+    new AotPlugin({
+      tsConfigPath: 'path/to/tsconfig.json',
+      entryModule: 'path/to/app.module#AppModule'
+    })
+  ]
+}
